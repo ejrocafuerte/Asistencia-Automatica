cb67f82f32a77786b5c4945f308c41216589f43a
ejrocafuerte <ejrocafuerte@gmail.com>
2017-11-28 23:06:24 -0500

    Actualizacion EMISOR
    agrego elementos extraidos del dispositivo, MAC, IMEI pendiente señal de dispositivo
    Problemas con API 15 migracion a API 17
    Manejo de la conexion bluetooth, encendido; pendiente establecer conexion y envio de datos

--- Emisor/app/src/main/java/com/asistencia/integradora/espol/emisor/MainActivity.java
+++ Emisor/app/src/main/java/com/asistencia/integradora/espol/emisor/MainActivity.java
@@ -1,13 +1,36 @@
 package com.asistencia.integradora.espol.emisor;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView txt_bt = (TextView) findViewById(R.id.txt_bt);
+        TextView txt_wifi = (TextView) findViewById(R.id.txt_mac);
+        TextView txt_imei = (TextView)findViewById(R.id.txt_imei);
+
+        txt_wifi.setText(BtConn.getMacAddress());
+        txt_imei.setText(BtConn.getIMEI(getApplicationContext()));
+
+        BluetoothAdapter btAdap;
+
+        btAdap = BluetoothAdapter.getDefaultAdapter();
+        if(btAdap!=null){
+            txt_bt.setText(btAdap.getAddress());
+            if(!btAdap.isEnabled()){
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, 1);
+            }
+
+        }
+
     }
 }
