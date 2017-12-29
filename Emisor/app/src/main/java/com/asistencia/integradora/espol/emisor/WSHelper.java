d5ac7464cb70e342c20ca856298c1e1627bc98fc
ejrocafuerte <ejrocafuerte@gmail.com>
2017-12-29 00:03:02 -0500

    SQL
    agrego archivos de base de datos local para almacenamiento de facultades, materias, paralelos y datos del profesor.
    Pendientes
    Pendiente consumo de webservice desde la aplicacion movil y conexion con la base de datos local

---
+++ Emisor/app/src/main/java/com/asistencia/integradora/espol/emisor/WSHelper.java
@@ -0,0 +1,14 @@
+package com.asistencia.integradora.espol.emisor;
+
+/**
+ * Created by erick on 28/12/2017.
+ */
+
+public class WSHelper {
+    public static String direccionWS ="https://ejrocafuerte.pythonanywhere.com/";
+    /*aqui debo de poner un login que devuelva un objeto profesor json*/
+    /*luego cargo facultades y materias desde el ws*/
+    public static void facultadesDesdeWS(){
+
+    }
+}
